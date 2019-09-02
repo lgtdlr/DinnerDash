93a65a181875705f17f121e450a95320e290211b
Josian Velez Ramos <josian.velez@upr.edu>
2019-09-02 14:02:36 -0400

    second base

---
+++ src/Resources/Images.java
@@ -0,0 +1,57 @@
+package Resources;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
+/**
+ * Created by AlexVR on 7/1/2018.
+ */
+public class Images {
+
+
+    public static BufferedImage[] butstart;
+    public static BufferedImage title;
+    public static BufferedImage Pause;
+    public static BufferedImage[] Resume;
+    public static BufferedImage[] BTitle;
+    public static BufferedImage[] Options;
+    public static ImageIcon icon;
+
+    public Images() {
+
+        butstart = new BufferedImage[3];
+        Resume = new BufferedImage[2];
+        BTitle = new BufferedImage[2];
+        Options = new BufferedImage[2];
+
+        try {
+
+            title = ImageIO.read(getClass().getResourceAsStream("/Sheets/Title.png"));
+            Pause = ImageIO.read(getClass().getResourceAsStream("/Sheets/Pause.png"));
+            butstart[0]= ImageIO.read(getClass().getResourceAsStream("/Buttons/NormBut.png"));//normbut
+            butstart[1]= ImageIO.read(getClass().getResourceAsStream("/Buttons/HoverBut.png"));//hoverbut
+            butstart[2]= ImageIO.read(getClass().getResourceAsStream("/Buttons/ClickedBut.png"));//clickbut
+
+            icon =  new ImageIcon(ImageIO.read(getClass().getResourceAsStream("/Sheets/icon.png")));
+
+
+        }catch (IOException e) {
+        e.printStackTrace();
+    }
+
+
+    }
+
+    public static BufferedImage loadImage(String path) {
+        try {
+            return ImageIO.read(Images.class.getResourceAsStream(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        return null;
+    }
+
+}
