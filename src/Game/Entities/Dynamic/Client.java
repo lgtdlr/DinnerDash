be57296f7ebeb31a55479b5d0ca37caac9c2f915
Jose A. Lopez Martínez <jose.lopez100@upr.edu>
2019-10-05 21:57:50 -0400

    Revert "Added full inspector functionality."
    This reverts commit 81b0c182d7c909e5462280886adb06e58825cddc.

--- src/Game/Entities/Dynamic/Client.java
+++ src/Game/Entities/Dynamic/Client.java
@@ -11,24 +11,14 @@ import java.awt.image.BufferedImage;
 import java.util.Random;
 
 public class Client extends BaseDynamicEntity {
-    private double patience;
-    private double OGpatience;
+    private int patience;
+    private int OGpatience;
     Order order;
     public boolean isLeaving = false;
-    private int runOnce=0;//To make condition run once in tick()
-    public int inspectorNotOnTime=0;//How many times inspector does not get food on time
-    
     public Client(int xPos, int yPos, Handler handler) {
-        super(Images.people[new Random().nextInt(10)], xPos, yPos,64,72, handler);
+        super(Images.people[new Random().nextInt(9)], xPos, yPos,64,72, handler);
         setPatience(new Random().nextInt(120*60)+60*60);
-        //Subtracting 6% patience to all future customers for each inspector not served
-        if(inspectorNotOnTime!=0)
-        	setOGpatience(getPatience()-getPatience()*0.06*inspectorNotOnTime);
-        else setOGpatience(getPatience());
-        //Adding 6% patience to all future customers for each inspector served
-        if(handler.getPlayer().inspectorOnTime!=0)
-        	setOGpatience(getPatience()*1.10*handler.getPlayer().inspectorOnTime);
-        
+        setOGpatience(getPatience());
         int numOfIngredients = new Random().nextInt(4)+1;
         int chumOrMeat = new Random().nextInt(2);
         order = new Order();
@@ -65,7 +55,6 @@ public class Client extends BaseDynamicEntity {
 
         }
         ((Burger) order.food).addIngredient(Item.topBread);
-        
 
     }
 
@@ -74,19 +63,6 @@ public class Client extends BaseDynamicEntity {
         if(getPatience()<=0){
             isLeaving=true;
         }
-        //Checks if inspector is served on time and if not, set money to 0
-		if(sprite.equals(Images.people[9])) {
-			if(isLeaving) {
-				handler.getPlayer().money=0;
-				inspectorNotOnTime++;
-			}
-		}
-		//Adding 12% patience to customers if inspector is served
-		if(handler.getPlayer().inspectorOnTime!=0 && runOnce==0) {
-			setPatience(getPatience()*1.12);
-			runOnce=1;
-		}
-			
     }
     public void render(Graphics g){
 
@@ -107,19 +83,19 @@ public class Client extends BaseDynamicEntity {
     	((Burger) order.food).y-=102;
     }
 
-	public double getPatience() {
+	public int getPatience() {
 		return patience;
 	}
 
-	public void setPatience(double patience) {
+	public void setPatience(int patience) {
 		this.patience = patience;
 	}
 
-	public double getOGpatience() {
+	public int getOGpatience() {
 		return OGpatience;
 	}
 
-	public void setOGpatience(double oGpatience) {
+	public void setOGpatience(int oGpatience) {
 		OGpatience = oGpatience;
 	}
 }
